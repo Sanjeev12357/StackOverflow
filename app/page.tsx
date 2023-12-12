18ccb209a55a9baecb2bce05f318e59fef6d5fb0
sanjeev saini <sanjeevsinghsaini48@gmail.com>
2023-12-12 10:10:40 +0530

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 13</h1>
-      <h2 className="h2-bold">next js</h2>
-      <h3 className="h3-bold"> next js</h3>
-    </div>
-  );
-}
