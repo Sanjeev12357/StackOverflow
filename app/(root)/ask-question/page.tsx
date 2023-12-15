a9d91f644d99b14dfb3929de987d673804107c0a
sanjeev saini <sanjeevsinghsaini48@gmail.com>
2023-12-15 11:47:47 +0530

    fix eslint errors

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,6 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-import { auth } from "@clerk/nextjs";
+// import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -15,7 +15,7 @@ const page = async () => {
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div>
-        <Question mongoUserId={JSON.stringify(mongoUser._id)}/>
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
       </div>
     </div>
   );
